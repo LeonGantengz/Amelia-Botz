a86ba8a90b6ebb4ccde69895108ce7f2f8626fc6
LeonGanz <100405315+LeonGantengz@users.noreply.github.com>
2022-04-18 15:54:56 +0700

    Update donasi.js

--- message/donasi.js
+++ message/donasi.js
@@ -5,10 +5,10 @@ return`
 *│* Bantu Developer ngembangin bot ini
 *│* dengan donasi seiklasnya 🙂
 *│*
-*│* Shopee : 085156137901
-*│* Gopay : 085156137901
-*│* Dana : 085156137901
-*│* Ovo : 085156137901
+*│* Shopee : 085608625102
+*│* Gopay : 085608625102
+*│* Dana : 085608625102
+*│* Ovo : 085608625102
 *│*
 *│* Makasih yang udah donasi
 *│* dan bantu ngembangin bot ini
